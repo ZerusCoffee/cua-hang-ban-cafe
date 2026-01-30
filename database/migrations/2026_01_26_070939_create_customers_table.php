f0da390b5abab99f17526da735f5e41eaa8d2eb8
hoang <hoangquy3001@gmail.com>
2026-01-30 20:42:41 +0700

    add: update avatar

--- database/migrations/2026_01_26_070939_create_customers_table.php
+++ database/migrations/2026_01_26_070939_create_customers_table.php
@@ -14,6 +14,7 @@ return new class extends Migration {
             $table->id();
             $table->string("name");
             $table->string("email")->unique(); // Cần unique để đăng nhập
+            $table->string("avatar")->nullable();
             $table->string("phone")->nullable();
             $table->string("password");
             $table->string("email_verified_at")->nullable();
